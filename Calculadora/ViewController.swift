f47acf72f26ffbe8db4bc4d056dea2d33f80c1a5
Mac13 <mac13@iMac-de-Mac13.local>
2015-01-20 16:52:57 -0600

    Initial Commit

---
+++ Calculadora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by Mac13 on 20/01/15.
+//  Copyright (c) 2015 fcc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
